6a4b851b1db830557c5de33e3f9b38d708efa826
Surinderpal singh <surinderkahlon1993@gmail.com>
2018-05-03 13:18:24 -0400

    added

---
+++ FinalInstagram/InstagramFinalProject/SearchImageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchImageTableViewController.h
+//  InstagramFinalProject
+//
+//  Created by Surinder Kahlon on 2018-03-31.
+//  Copyright © 2018 surinder pal singh sidhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchImageTableViewController : UITableViewController
+
+@end
